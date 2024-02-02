c98f81d16495742541b7e858c2595e0e910267ab
taileduc0404 <taileduc0404@gmail.com>
2024-02-02 21:56:47 +0700

    Add DTOs, Features, AutoMapper, MediatR.

---
+++ src/Core/Application/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+	public class LeaveTypeDto
+	{
+		public string Name { get; set; } = string.Empty;
+		public string DefaultDay { get; set; } = string.Empty;
+	}
+}
---
+++ src/Core/Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using AutoMapper;
+using LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.MappingProfiles
+{
+	public class LeaveTypeProfile : Profile
+	{
+		public LeaveTypeProfile()
+		{
+			CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+		}
+	}
+}
