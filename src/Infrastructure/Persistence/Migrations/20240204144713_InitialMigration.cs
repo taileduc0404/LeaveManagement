e2e39f14a83ba825061b59f551184d61e62b030d
taileduc0404 <taileduc0404@gmail.com>
2024-02-04 21:48:34 +0700

    Add-Migration And Update Database.

---
+++ src/Infrastructure/Persistence/Migrations/20240204144713_InitialMigration.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "leaveTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DefaultDay = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_leaveTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "leaveAllocations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberOfDays = table.Column<int>(type: "int", nullable: false),
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    Period = table.Column<int>(type: "int", nullable: false),
+                    EmployeeId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_leaveAllocations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_leaveAllocations_leaveTypes_LeaveTypeId",
+                        column: x => x.LeaveTypeId,
+                        principalTable: "leaveTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "leaveRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    DateRequested = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RequestComments = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Approved = table.Column<bool>(type: "bit", nullable: false),
+                    Cancled = table.Column<bool>(type: "bit", nullable: false),
+                    RequestingEmployeeId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_leaveRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_leaveRequests_leaveTypes_LeaveTypeId",
+                        column: x => x.LeaveTypeId,
+                        principalTable: "leaveTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "leaveTypes",
+                columns: new[] { "Id", "DateCreated", "DateModified", "DefaultDay", "Name" },
+                values: new object[] { 1, new DateTime(2024, 2, 4, 21, 47, 12, 984, DateTimeKind.Local).AddTicks(7981), new DateTime(2024, 2, 4, 21, 47, 12, 984, DateTimeKind.Local).AddTicks(7997), 10, "Vacation" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_leaveAllocations_LeaveTypeId",
+                table: "leaveAllocations",
+                column: "LeaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_leaveRequests_LeaveTypeId",
+                table: "leaveRequests",
+                column: "LeaveTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "leaveAllocations");
+
+            migrationBuilder.DropTable(
+                name: "leaveRequests");
+
+            migrationBuilder.DropTable(
+                name: "leaveTypes");
+        }
+    }
+}
