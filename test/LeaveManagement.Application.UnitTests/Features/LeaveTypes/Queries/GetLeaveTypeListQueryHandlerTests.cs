866640685cc2e3fec2f7ade5dcd0b52135c8004b
taileduc0404 <taileduc0404@gmail.com>
2024-02-06 10:06:22 +0700

    Unit Testing the Application Code - Testing Handlers.

---
+++ test/LeaveManagement.Application.UnitTests/Features/LeaveTypes/Queries/GetLeaveTypeListQueryHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using Application.Contracts.Logging;
+using Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using Application.MappingProfiles;
+using AutoMapper;
+using LeaveManagement.Application.Contracts.Persistences;
+using LeaveManagement.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+
+namespace LeaveManagement.Application.UnitTests.Features.LeaveTypes.Queries
+{
+	public class GetLeaveTypeListQueryHadlerTests
+	{
+		private readonly Mock<ILeaveTypeRepository> _mockRepo;
+		private IMapper _mapper;
+		private Mock<IAppLogger<GetAllLeaveTypeQueryHandler>> _mockAppLogger;
+		public GetLeaveTypeListQueryHadlerTests()
+		{
+			_mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+
+			var mapperConfig = new MapperConfiguration(c =>
+			{
+				c.AddProfile<LeaveTypeProfile>();
+			});
+			_mapper = mapperConfig.CreateMapper();
+			_mockAppLogger = new Mock<IAppLogger<GetAllLeaveTypeQueryHandler>>();
+		}
+
+		[Fact]
+		public async Task GetLeaveTypeListTest()
+		{
+			var handler = new GetAllLeaveTypeQueryHandler(_mapper, _mockRepo.Object, _mockAppLogger.Object);
+			var result = await handler.Handle(new GetAllLeaveTypesQuery(), CancellationToken.None);
+			result.ShouldBeOfType<List<LeaveTypesDto>>();
+			result.Count.ShouldBe(3);
+
+		}
+	}
+
+	//public class GetLeaveTypeListQueryHandlerTests
+	//{
+	//	private readonly Mock<ILeaveTypeRepository> _mockRepo;
+	//	private IMapper _mapper;
+	//	private Mock<IAppLogger<GetAllLeaveTypeQueryHandler>> _mockAppLogger;
+
+	//	public GetLeaveTypeListQueryHandlerTests()
+	//	{
+	//		_mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+
+	//		var mapperConfig = new MapperConfiguration(c =>
+	//		{
+	//			c.AddProfile<LeaveTypeProfile>();
+	//		});
+
+	//		_mapper = mapperConfig.CreateMapper();
+	//		_mockAppLogger = new Mock<IAppLogger<GetAllLeaveTypeQueryHandler>>();
+	//	}
+
+	//	[Fact]
+	//	public async Task GetLeaveTypeListTest()
+	//	{
+	//		var handler = new GetAllLeaveTypeQueryHandler(_mapper, _mockRepo.Object, _mockAppLogger.Object);
+
+	//		var result = await handler.Handle(new GetAllLeaveTypesQuery(), CancellationToken.None);
+
+	//		result.ShouldBeOfType<List<LeaveTypesDto>>();
+	//		result.Count.ShouldBe(3);
+	//	}
+	//}
+}
---
+++ test/LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,50 @@
+﻿using Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using LeaveManagement.Application.Contracts.Persistences;
+using LeaveManagement.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.UnitTests.Mocks
+{
+	public class MockLeaveTypeRepository
+	{
+		public static Mock<ILeaveTypeRepository> GetMockLeaveTypeRepository()
+		{
+			var leaveTypes = new List<LeaveType>
+			{
+				new LeaveType
+				{
+					Id = 1,
+					DefaultDay=10,
+					Name="Test Vacation"
+				},
+				new LeaveType
+				{
+					Id = 2,
+					DefaultDay = 20,
+					Name = "Test Sick"
+				},
+				new LeaveType
+				{
+					Id = 3,
+					DefaultDay = 30,
+					Name = "Test Maternity"
+				}
+			};
+			var mockRepo = new Mock<ILeaveTypeRepository>();
+			mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(leaveTypes);
+
+			mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+				.Returns((LeaveType LeaveType) =>
+				{
+					leaveTypes.Add(LeaveType);
+					return Task.CompletedTask;
+				});
+			return mockRepo;
+		}
+	}
+}
